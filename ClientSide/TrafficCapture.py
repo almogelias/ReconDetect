1705201e8bdbc4b1b449f3101507e7ed110c53c2
almogelias <57565283+almogelias@users.noreply.github.com>
2019-12-28 20:20:37 +0200

    Update TrafficCapture.py

--- ClientSide/TrafficCapture.py
+++ ClientSide/TrafficCapture.py
@@ -8,34 +8,43 @@ from scapy.all import *
 import psutil
 from adapterInfo import Adapter
 from parsePacket import Parser
+from Flow import Flow
 import os
 import datetime
 
-
 ### Global Variables ###
-filter = 'ip or arp or icmp'
+filter = "tcp or udp"
 
 
 ### CallBack of Sniffer ###
 def pkt_callback(pkt):
-    #pkt.show() # debug statement - Full RawDATA
-    #print(pkt.summary()) #debug Smaller RawDATA
+    # pkt.show() # debug statement - Full RawDATA
+    # print(pkt.summary()) #debug Smaller RawDATA
     parser = Parser(pkt)
+    '''
+    print("----------------------------")
+    print(flow.FlowsToString())
+    print("----------------------------")
+    '''
     parser.toStringPrint()
-    createLogFile(writePath,fileNameWithTime,parser)
-
+    flow.addPacket(parser.dataDict)
+    createLogFile(writePath, fileNameWithTime, parser)
 
     ###Writing to file###
-def createLogFile(writepath,fileNameWithTime,parser):
+
+
+def createLogFile(writepath, fileNameWithTime, parser):
     # define the name of the file to be created
     mode = 'a' if os.path.exists(writepath) else 'w'
-    with open(writepath+'\\'+fileNameWithTime, mode) as f:
-        f.write(parser.toStringLog()+'\n')
+    with open(writepath + '\\' + fileNameWithTime, mode) as f:
+        f.write(parser.toStringLog() + '\n')
+
 
 ### Create A Folder ###
 def createFolder(writepath):
     # define the name of the directory to be created
-    fileNameWithTime = fileName + '_' + str(int(datetime.datetime.now().timestamp())) + '.txt'  # Get File with Unix time
+    fileNameWithTime = fileName + '_' + str(
+        int(datetime.datetime.now().timestamp())) + '.txt'  # Get File with Unix time
     try:
         os.mkdir(writepath)
     except OSError:
@@ -54,29 +63,33 @@ def get_networkAdapter():
 
     available_networks = []
     for intface, addr_list in addresses.items():
-        #if ("local area network" in intface.lower() or "wireless network connection" in intface.lower() or "Local Area Connection" in intface): #Search for Local Area Network name or Wireless adapter.
-        #if ("local area network" in intface.lower() or "wireless network connection" in intface.lower() or "VMware Network Adapter VMnet8" in intface):  # Search for Local Area Network name or Wireless adapter.
-        if intface in stats and getattr(stats[intface], "isup"): #Get all the adapter which is up
-            available_networks.append(Adapter(intface,addr_list[1].address,addr_list[1].netmask)) #Write Adapter info from adapterInfo.py
+        # if ("local area network" in intface.lower() or "wireless network connection" in intface.lower() or "Local Area Connection" in intface): #Search for Local Area Network name or Wireless adapter.
+        # if ("local area network" in intface.lower() or "wireless network connection" in intface.lower() or "VMware Network Adapter VMnet8" in intface):  # Search for Local Area Network name or Wireless adapter.
+        if intface in stats and getattr(stats[intface], "isup"):  # Get all the adapter which is up
+            available_networks.append(
+                Adapter(intface, addr_list[1].address, addr_list[1].netmask))  # Write Adapter info from adapterInfo.py
             print(addr_list[1].address)
         else:
             continue
-    return(available_networks)
+    return (available_networks)
 
 
 #### MAIN Func###
-
+flow = Flow()
 fileName = 'logfile'
 writePath = 'C:\\temp'
-fileNameWithTime=createFolder(writePath)
+fileNameWithTime = createFolder(writePath)
 
 for onlineAdapter in get_networkAdapter():
-   onlineAdapter.toString()
-   print(onlineAdapter.ipNetworkWithPrefix())
+    onlineAdapter.toString()
+    print(onlineAdapter.ipNetworkWithPrefix())
 
-### Calling Sniffer Driver (Scapy Lib)
-   sniff(iface=onlineAdapter.name, prn=pkt_callback, filter=filter +" "+"and src host {} and dst net {}".format(onlineAdapter.ip,onlineAdapter.ipNetworkWithPrefix()))
-   #sniff(iface=onlineAdapter.name, prn=pkt_callback,filter=filter) #Sniff All !
+    ### Calling Sniffer Driver (Scapy Lib)
+    # sniff(iface=onlineAdapter.name, prn=pkt_callback, filter=filter +" "+"and src host {} and dst net {}".format(onlineAdapter.ip,onlineAdapter.ipNetworkWithPrefix()))
+    sniff(iface=onlineAdapter.name, prn=pkt_callback,
+              filter=filter + " " + "and src net {} and dst net {} and not dst host 192.168.222.255".format(
+                  onlineAdapter.ipNetworkWithPrefix(), onlineAdapter.ipNetworkWithPrefix()))  # Sniff All !
+    # sniff(iface=onlineAdapter.name, prn=pkt_callback, filter="tcp or icmp and src net {} and dst net {}".format(onlineAdapter.ipNetworkWithPrefix(),
+    #        onlineAdapter.ipNetworkWithPrefix()), store=0, timeout=40)     #Capture filter TCP-Syn or ICMP and HostIP and dest=Network
 
-   #sniff(iface=onlineAdapter.name, prn=pkt_callback, filter="tcp or icmp and src net {} and dst net {}".format(onlineAdapter.ipNetworkWithPrefix(),
-     #        onlineAdapter.ipNetworkWithPrefix()), store=0, timeout=40)     #Capture filter TCP-Syn or ICMP and HostIP and dest=Network
+
