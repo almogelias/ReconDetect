a1a858708a72d537eef7d10539fae2bbc6220df2
almogelias <57565283+almogelias@users.noreply.github.com>
2020-01-05 21:03:58 +0200

    Update Server.py

--- ClientSide/Simulated/Flow/Server.py
+++ ClientSide/Simulated/Flow/Server.py
@@ -1,21 +1,30 @@
 import socket
 
-PORTLIST = [445,3389,21,23]
-for port in PORTLIST:
-    ds = ("0.0.0.0", port)
+port = 3389
 
-    serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serv.bind(('0.0.0.0', port))
-    serv.listen(1)
-    print('socket binded to port', port)
+
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serv.bind(('0.0.0.0', port))
+
+serv.listen(1)
+
+print('socket binded to port', port)
+
+
 
 serv.listen(5)
+
 print("socket is listening")
 
+
 while True:
+
     conn, addr = serv.accept()
+
     from_client = ''
+
+
 
     while True:
         data = conn.recv(4096)
@@ -25,6 +34,6 @@ while True:
         print (from_client)
         msg = "I am SERVER\n".encode('utf-8')
         conn.send(msg)
-
     conn.close()
+
     print ('client disconnected')
