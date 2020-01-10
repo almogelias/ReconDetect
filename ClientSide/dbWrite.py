45cf24dae247457265b667ffde00ab91820a2577
almogelias <57565283+almogelias@users.noreply.github.com>
2020-01-10 13:56:28 +0200

    Update dbWrite.py

--- ClientSide/dbWrite.py
+++ ClientSide/dbWrite.py
@@ -1,18 +1,17 @@
 import pyodbc
 
 Driver = 'ODBC Driver 17 for SQL Server'
-Server = 'DESKTOP-GRTRD9G'
+Server = 'LabSRV'
 DB = 'FinalProject'
 
 
 def connectSQLServer(driver, server, db):
-
     connSQLServer = pyodbc.connect(
         r'DRIVER={' + driver + '};'
-        r'SERVER=' + server + ';'
-        r'DATABASE=' + db + ';'
-        r'UID=sa;'
-        r'PWD=Ll123456'
+                               r'SERVER=' + server + ';'
+                                                     r'DATABASE=' + db + ';'
+                                                                         r'UID=sa;'
+                                                                         r'PWD=Ll123456'
 
     )
     return connSQLServer
@@ -22,47 +21,54 @@ class Database():
     def __init__(self):
         self.conn = connectSQLServer(Driver, Server, DB)
 
-    def insertDataTime(self, dataDict, curs):
-        query = """INSERT INTO  dbo.Times values({},{},'{}','{}')""".format(dataDict["UnixTimeMicrosec"],
-                                                                            dataDict["Time"],
-                                                                            dataDict["Date"],
-                                                                            dataDict["Day"])
-
-        curs.execute(query)
-        curs.commit()
-
-    def insertDataPacket(self, dataDict, curs):
-        query = """INSERT INTO  dbo.Packets values({},'{}','{}',{},{},{},{},{},{},'{}',{},{},{},'{}',{},{})""".format(
-            dataDict["UnixTimeMicrosec"],
-            dataDict["srcIpAddr"],
-            dataDict["dstIpAddr"],
-            dataDict["IPLen"],
-            dataDict["ID"],
-            dataDict["Ttl"],
-            dataDict["IPChksum"],
-            dataDict["srcPort"],
-            dataDict["dstPort"],
-            dataDict["service"],
-            dataDict["Seq"],
-            dataDict["Ack"],
-            dataDict["dataOfs"],
-            dataDict["Flags"],
-            dataDict["Window"],
-            dataDict["TcpChksum"]
-        )
-
-        curs.execute(query)
-        curs.commit()
-
-    def insertDataFile(self, dataDict, curs):
-        query = """INSERT INTO  dbo.Files values({},{},'{}','{}',{})""".format(dataDict["UnixTimeMicrosec"],
-                                                                               dataDict["PID"],
-                                                                               dataDict["fileName"],
-                                                                               dataDict["filePath"],
-                                                                               dataDict["deltaTimeMillisec"])
-
-        curs.execute(query)
-        curs.commit()
+    def insertFlows(self, flows):
+        curs = self.conn.cursor()
+        for flow in flows:
+            try:
+                query="""SELECT FlowID FROM [FinalProject].[dbo].[Flows] 
+                            Where ((srcIpAddr = '{}' and dstIpAddr='{}'
+                            and Service='{}' and srcPort='{}' and dstPort='{}')
+                            or
+                            (srcIpAddr = '{}' and dstIpAddr='{}'
+                            and Service='{}' and srcPort='{}' and dstPort='{}'))
+                            and
+                            counterOfFin=0
+                            and
+                            (({}-UnixEndTimeMillisec)/(1000))<120;""".format(
+                    flow["srcIpAddr"],
+                    flow["dstIpAddr"],
+                    flow["Service"],
+                    flow["srcPort"],
+                    flow["dstPort"],
+                    flow["srcIpAddr"],
+                    flow["dstIpAddr"],
+                    flow["Service"],
+                    flow["srcPort"],
+                    flow["dstPort"],
+                    flow['UnixStartTimeMillisec']
+                )
+                results = curs.execute(query)
+                print (results)
+            except:
+                query = """INSERT INTO  dbo.Flows values({},{},{},'{}','{}','{}','{}',{},{},{},{},{},{},{},{})""".format(
+                    flow["UnixStartTimeMillisec"],
+                    flow["UnixEndTimeMillisec"],
+                    flow["Time"],
+                    flow["Day"],
+                    flow["srcIpAddr"],
+                    flow["dstIpAddr"],
+                    flow["Service"],
+                    flow["srcPort"],
+                    flow["dstPort"],
+                    flow["counterOfPackets"],
+                    flow["counterOfSyn"],
+                    flow["counterOfPa"],
+                    flow["counterOfR"],
+                    flow["counterOfRA"],
+                    flow["counterOfFin"]
+                )
+                curs.execute(query)
+                curs.commit()
 
     def insertCountsTenSecond(self, lastPacketCounting):
         curs = self.conn.cursor()
@@ -107,12 +113,8 @@ class Database():
     def insertData(self, dataDict):
         curs = self.conn.cursor()
         try:
-            self.insertCountsTenSecond(dataDict,curs)
-            #self.insertDataTime(dataDict, curs)
-            #self.insertDataPacket(dataDict, curs)
-            #if (dataDict["PID"] != 'None'):
-            #    if (dataDict["deltaTimeMillisec"] != "None"):
-            #        self.insertDataFile(dataDict, curs)
+            self.insertCountsTenSecond(dataDict, curs)
+
         except Exception as e:
             print(e)
             curs.rollback()
